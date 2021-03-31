9ff55b674522abe28e27b87f5bbfea3519e942bd
Edilson Rodrigues <edarodrigs@gmail.com>
2021-03-31 09:31:57 -0300

    [dt-money][modal-form]styled modal

--- reactjs/dtmoney/src/App.tsx
+++ reactjs/dtmoney/src/App.tsx
@@ -2,12 +2,32 @@ import {GlobalStyle} from "./styles/global";
 
 import {Header} from "./component/Header";
 import {Dashboard} from "./component/Dashboard";
+import Modal from "react-modal";
+import {useState} from "react";
+import {NewTransactionModal} from "./component/NewTransactionModal";
+
+Modal.setAppElement('#root');
 
 export function App() {
+
+    const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState<boolean>(false);
+
+    function handleOpenNewTransactionModal() {
+        setIsNewTransactionModalOpen(true);
+    }
+
+    function handleCloseNewTransactionModal() {
+        setIsNewTransactionModalOpen(false);
+    }
+
     return (
         <>
-            <Header/>
+            <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
             <Dashboard/>
+            <NewTransactionModal
+                isOpen={isNewTransactionModalOpen}
+                onRequestClose={handleCloseNewTransactionModal}
+            />
             <GlobalStyle/>
         </>
     );
